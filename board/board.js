fc37aac73be983a7fe5519c10fc747d825f37337
schmax <stark.m.2003@gmail.com>
2023-04-12 14:54:53 +0200

    jupstart

---
+++ board/board.js
@@ -0,0 +1,75 @@
+window.onresize = changeContentOnWindowSize;
+
+// var let const warscheinlich werden alle for loops nacher mit der api gesynct
+
+const trueboardTaskcard = document.getElementsByClassName('trueboard-taskcard');
+
+// var let const warscheinlich werden alle for loops nacher mit der api gesynct
+
+function consoleLOGS() {
+    console.log(tasks);
+}
+
+function changeContentOnWindowSize() {
+    const mediaQuery1100px = window.matchMedia('(max-width: 1100px)')
+    const mediaQuery900px = window.matchMedia('(max-width: 900px)')
+
+
+    if (mediaQuery1100px.matches) {
+        document.getElementById('board-AddtaskRe').style = ("display: flex;")
+        document.getElementById('board-Addtask').style = ("display: none;")
+    } else {
+        document.getElementById('board-AddtaskRe').style = ("display: none;")
+        document.getElementById('board-Addtask').style = ("display: flex;")
+    }
+
+    if (mediaQuery900px.matches) {
+        document.getElementById('board-kanban').style = ("display: flex;")
+
+    } else {
+        document.getElementById('board-kanban').style = ("display: none;")
+
+    }
+}
+
+function test() {
+    alert("jadi jadi jada ")
+}
+
+// Warum cardInspect cardUnInspect weil ohne framework uh library gibt es keinen anderen weg //
+
+function cardInspect() {
+
+    for (let i = 0; i < trueboardTaskcard.length; i++) {
+        trueboardTaskcard[i].style.backgroundColor = '#bebebf';
+    }
+
+    document.getElementById('body').style = ("background-color: #b8b9ba; position: fixed;")
+    document.getElementById('taskcard-inspect').style = ("display: flex;")
+    document.getElementById('fullviewport').style = ("display: flex;")
+    document.getElementById('thebnt').style = ("display: flex;")
+    document.getElementsByClassName('trueboard-taskcard').style = ('background-color: #bebebf;')
+    document.getElementById('board-Addtask').style = ('background-color: #1e2834')
+    document.getElementById('board-Addtask-text').style = ('color: #bfbebe')
+    document.getElementById('board-Addtask-plusicon-line').style = ('background-color: ##bfbebe')
+    document.getElementById('board-Addtask-plusicon-linev').style = ('background-color: #bfbebe')
+    document.getElementById('board-Addtask').style = ("display: none;")
+    document.getElementById('trueboard-userIco-start').style = ("background-color: #bfbebe")
+    console.log("dadada")
+}
+
+function cardUnInspect() {
+
+    for (let i = 0; i < trueboardTaskcard.length; i++) {
+        trueboardTaskcard[i].style = '';
+    }
+
+    document.getElementById('body').style = ("background-color: #F6F7F8;")
+    document.getElementById('taskcard-inspect').style = ("display: none;")
+    document.getElementById('fullviewport').style = ("display: none;")
+    document.getElementById('thebnt').style = ("display: none;")
+    document.getElementById('board-Addtask').style = ('background-color: #2A3647')
+    document.getElementById('board-Addtask-text').style = ('color: #FFFFFF')
+    document.getElementById('board-Addtask-plusicon').style = ('background-color: #FFFFFF')
+    changeContentOnWindowSize();
+}
---
+++ up.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo branch to commit to ?
+read branch
+echo commit msg ?
+read commitmsg
+read -p "merge main ? [Y/n/M] " prompt
+
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+    echo yes
+    git switch $branch
+    git pull 
+    git add . 
+    git commit -m "$commitmsg" 
+    git push 
+    git switch main 
+    git pull  
+    git merge $branch 
+    git push 
+    git switch $branch 
+    git merge main 
+    git push
+else
+    if [[ $prompt == "M" || $prompt == "m" ]]; then
+        echo merge 
+        git switch $branch
+        git pull 
+        echo the branch you want $branch to merge to
+        read $tobranch
+        git merge $tobranch 
+        git push
+    else
+        echo no
+        git switch $branch
+        git pull
+        git add . 
+        git commit -m "$commitmsg" 
+        git push 
+    fi
+fi
