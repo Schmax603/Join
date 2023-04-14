3ab25b1f978b93bed94dd96e089fb2fefa4e8491
Zwogli <Mathiaskohler@web.de>
2023-04-14 12:40:15 +0200

    animate drdo menu

--- headerNav.js
+++ headerNav.js
@@ -1,3 +1,13 @@
+function toggleMenu(){
+  let headerMenu = document.getElementById('mobile-drdo-menu');
+  let body = document.getElementById('body');
+
+  body.classList.toggle('of-hidden')
+  headerMenu.classList.toggle('d-none')
+  headerMenu.classList.add('animation')
+
+}
+
 function toggleOverlays(className){  
   let callDocument = document.getElementById(`${className}`);
 
