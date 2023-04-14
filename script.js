45e95bfee807c0e99e6c1981c569998da915b01c
schmax <stark.m.2003@gmail.com>
2023-04-14 14:33:58 +0200

    upsh backend go to work

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let users = [];
 setURL('https://gruppe-534.developerakademie.net/smallest_backend_ever');
 
+
+function log() {
+    console.log('Loaded', result);
+}
+
 async function init() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
--- up.sh
+++ up.sh
@@ -5,20 +5,56 @@ echo commit msg ?
 read commitmsg
 read -p "merge main ? [Y/n/M] " prompt
 
+
+# ----------------------------------
+# Colors
+# ----------------------------------
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+sleep 120
+
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
     echo yes
+    echo ${BLUE} git switch $branch ${NOCOLOR}
     git switch $branch
+    echo ${BLUE} git pull ${NOCOLOR}
     git pull 
+    echo ${BLUE} git add . ${NOCOLOR}
     git add .
+    echo ${BLUE} git commit -m "$commitmsg" ${NOCOLOR}
     git commit -m "$commitmsg" 
+    echo ${BLUE} git push ${NOCOLOR}
     git push
+    echo ${BLUE} git switch main ${NOCOLOR}
     git switch main
+    echo ${BLUE} git pull ${NOCOLOR}
     git pull
+    echo ${BLUE} git merge $branch ${NOCOLOR}
     git merge $branch 
+    echo ${BLUE} git push ${NOCOLOR}
     git push
+    echo ${BLUE} git ftp push ${NOCOLOR}
     git ftp push
+    echo ${BLUE} git switch $branch ${NOCOLOR}
     git switch $branch 
+    echo ${BLUE} git merge main ${NOCOLOR}
     git merge main 
+    echo ${BLUE} git push ${NOCOLOR}
     git push
     echo fertig
     sleep 120
