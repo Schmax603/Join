a6c6883d205b8550f702e9588c35fcd298ee5003
schmax <stark.m.2003@gmail.com>
2023-04-14 14:00:08 +0200

    The Purge

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let users = [];
-setURL('http://gruppe-534.developerakademie.net/smallest_backend_ever');
+setURL('https://gruppe-534.developerakademie.net/smallest_backend_ever');
 
 async function init() {
     await downloadFromServer();
