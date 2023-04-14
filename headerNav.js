eccf2a91bfee8e147e77cb0d5b21c0a38e4ba9c8
Zwogli <Mathiaskohler@web.de>
2023-04-14 12:46:38 +0200

    fix drdo menu

--- headerNav.js
+++ headerNav.js
@@ -1,19 +1,27 @@
+let checkMenu = false;
+
 function toggleMenu(){
   let headerMenu = document.getElementById('mobile-drdo-menu');
   let body = document.getElementById('body');
 
-  body.classList.toggle('of-hidden')
-  headerMenu.classList.toggle('d-none')
-  headerMenu.classList.add('animation')
-
+  body.classList.toggle('of-hidden');
+  headerMenu.classList.toggle('d-none');
+  headerMenu.classList.add('animation');
+  checkMenu = !checkMenu;
 }
 
 function toggleOverlays(className){  
   let callDocument = document.getElementById(`${className}`);
+  let headerMenu = document.getElementById('mobile-drdo-menu');
 
   callDocument.classList.toggle('d-none')
   if (className == 'help') {
     let helpIcon = document.getElementById('header-help-icon');
     helpIcon.classList.toggle('d-none');
+  }
+  if (checkMenu == true) {
+    headerMenu.classList.toggle('d-none');
+    headerMenu.classList.add('animation');
+    checkMenu = !checkMenu;
   }
 }
