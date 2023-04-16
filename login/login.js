fa811c9f116ac6331324ac2613c77a8ecf5ed81b
Zwogli <Mathiaskohler@web.de>
2023-04-16 07:45:40 +0200

    fix responisive & animation Logo white add btn hover effekt

--- login/login.js
+++ login/login.js
@@ -1,5 +1,10 @@
 function initLogin(){
   let overlayCard = document.getElementById('login-card');
+  let whiteLogo = document.getElementById('logo-white');
+
+  setTimeout(function(){ 
+    whiteLogo.classList.add('d-none');
+  }, 1000);
 
   overlayCard.innerHTML = /*html*/`
         <div class="overlay-login-header">
@@ -23,8 +28,8 @@ function initLogin(){
           </div>
 
           <div class="overlay-login-buttons">
-            <button class="bg-c-db cursor-p fc-w fs-21-700">Log in</button>
-            <button class="bg-c-w cursor-p fs-21-700">Guest Log in</button>
+            <button class="btn-dark bg-c-db cursor-p fc-w fs-21-700">Log in</button>
+            <button class="btn-bright bg-c-w cursor-p fs-21-700">Guest Log in</button>
           </div>
 
         </div>
@@ -38,9 +43,9 @@ function openSignUp(){
   loginCard.innerHTML = '';
 
   loginCard.innerHTML = /*html*/`
-     <img onclick="closeSignup()" class="overlay-login-arrow" src="../img/arrow_left_blue.png" alt="arrow left blue">
+     <img onclick="closeSignup()" class="overlay-login-arrow cursor-p" src="../img/arrow_left_blue.png" alt="arrow left blue">
         <div class="overlay-login-header">
-          <h1 class="cursor-d fs-62-700">Sign up</h1>
+          <h1 class="cursor-d fs-62-700 ta-c">Sign up</h1>
           <div class="border"></div>
         </div>
 
@@ -53,7 +58,7 @@ function openSignUp(){
         <div class="overlay-login-selection">
 
           <div class="overlay-login-buttons">
-            <button class="bg-c-db cursor-p fc-w fs-21-700">Sign up</button>
+            <button class="btn-dark bg-c-db cursor-p fc-w fs-21-700">Sign up</button>
           </div>
         </div>
   `;
@@ -74,19 +79,19 @@ function openForgotPassword(){
   loginCard.innerHTML = '';
 
   loginCard.innerHTML = /*html*/`
-      <img onclick="closeSignup()" class="overlay-login-arrow" src="../img/arrow_left_blue.png" alt="arrow left blue">
+      <img onclick="closeSignup()" class="overlay-login-arrow cursor-p" src="../img/arrow_left_blue.png" alt="arrow left blue">
         <div class="overlay-login-header header-forgot-password">
-          <h1 class="cursor-d fs-62-700">I forgot my password</h1>
+          <h1 class="cursor-d fs-62-700 ta-c">I forgot my password</h1>
           <div class="border"></div>
         </div>
 
         <form class="overlay-login-form">
-          <p class="fs-21-400 ta-c mb-35">Don´t worry! We will send you an email with the instructions to reset your password.</p>
+          <p class="cursor-d fs-21-400 ta-c mb-35">Don´t worry! We will send you an email with the instructions to reset your password.</p>
           <input type="email" placeholder="Email" required>
         </form>
 
           <div class="overlay-login-buttons">
-            <button class="btn-send bg-c-db cursor-p fc-w fs-21-700">Send me the email</button>
+            <button class="btn-dark btn-send bg-c-db cursor-p fc-w fs-21-700">Send me the email</button>
           </div>
   `;
 }
