c41bd601a5b66c946e3d76397a82bc71e1fb3305
Zwogli <Mathiaskohler@web.de>
2023-04-18 12:11:46 +0200

    add animation

--- login/passwordReset.js
+++ login/passwordReset.js
@@ -1,3 +1,20 @@
 function newPassword(){
   let userEmail = document.getElementById('requesterEmail');
 }
+
+function initMsgPassword(){
+  let overlayMsgBox = document.getElementById('overlay-msg-password');
+  let msgBox = document.getElementById('msg-box-password');
+  let body = document.getElementById('body-password');
+
+  msgBox.classList.remove('d-none')
+  overlayMsgBox.classList.add('overlay-msg');
+  msgBox.classList.add('animate-msg');
+  body.classList.add('overflow-h');
+
+  setTimeout(() => {
+    overlayMsgBox.classList.remove('overlay-msg');
+    body.classList.remove('overflow-h');
+    msgBox.classList.add('d-none')
+  }, 2000);
+}
