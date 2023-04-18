d6c93c9cebeb85280909c3421bf7e569e7862a18
Zwogli <Mathiaskohler@web.de>
2023-04-18 11:50:22 +0200

    edit forgotPasswordSucces

--- forgotPasswordSucces/forgotPasswordSucces.js
+++ forgotPasswordSucces/forgotPasswordSucces.js
@@ -1,5 +1,15 @@
 function initMsg(){
+  let overlayMsgBox = document.getElementById('overlay-msg');
+  let msgBox = document.getElementById('msg-box');
+  let body = document.getElementById('body');
+
+  overlayMsgBox.classList.add('overlay-msg');
+  msgBox.classList.add('animate-msg');
+  body.classList.add('overflow-h');
+
   setTimeout(() => {
-    document.getElementById('msg-box').classList.add('animate-msg');
+    overlayMsgBox.classList.remove('overlay-msg');
+    msgBox.classList.add('d-none')
+    body.classList.remove('overflow-h');
   }, 4000);
 }
