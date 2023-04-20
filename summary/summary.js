d552933c1bbe3f743416ca4bda877bacc6122807
Zwogli <Mathiaskohler@web.de>
2023-04-20 11:28:11 +0200

    ass summary greetings

---
+++ summary/summary.js
@@ -0,0 +1,41 @@
+let timeOfDay;
+
+/**
+ * Generate time of Day 
+ */
+function renderTimeOfDay(){
+  let date = new Date;
+  let hours = date.getHours();
+  if(hours > 7 && hours < 13){
+    timeOfDay = 'Good morning';
+  }else if(hours > 13 && hours < 18){
+    timeOfDay = 'Good afternoon';
+  }else if(hours > 18 && hours < 22){
+    timeOfDay = 'Good evening';
+  }
+  else if(hours > 22 && hours < 24 || hours > 0 && hours < 7){
+    timeOfDay = 'Good night';
+  }
+}
+
+/**
+ * Generate greetings for user
+ */
+async function renderGreeting(){
+  let greetings = document.getElementById('summary-infos-greeting');
+  let mobileGreeting = document.getElementById('mobile-overlay');
+
+  await init();
+  let activUser = currentUser.currentUser;
+  renderTimeOfDay();
+
+  greetings.innerHTML = '';
+  mobileGreeting.innerHTML = '';
+
+  if(activUser == ''){
+    generateHTMLGreetingGuest(greetings, mobileGreeting)
+  }else{
+    generateHTMLGreetingUser(greetings, mobileGreeting, activUser)
+  }
+  
+}
--- summary/summaryTemplates.js
+++ summary/summaryTemplates.js
@@ -1,25 +1,19 @@
-let timeOfDay;
-
-function renderTimeOfDay(){
-  let date = new Date;
-  let hours = date.getHours();
-  if(hours > 7 && hours < 13){
-    timeOfDay = 'Good morning';
-  }else if(hours > 13 && hours < 18){
-    timeOfDay = 'Good afternoon';
-  }else if(hours > 18 && hours < 22){
-    timeOfDay = 'Good evening';
-  }
-  else if(hours > 22 && hours < 24 || hours > 0 && hours < 7){
-    timeOfDay = 'Good night';
-  }
+function generateHTMLGreetingGuest(greetings, mobileGreeting){
+greetings.innerHTML = /*html*/`
+<p class="font-text-47">${timeOfDay}</p>
+`;
+return mobileGreeting.innerHTML = /*html*/`
+<p class="font-text-36">${timeOfDay}</p>
+`;
 }
 
-function generateHtmlGuest(){
-  let greetings = document.getElementById('summary-infos-greeting');
-
-  // greetings.innerHTML = '';
+function generateHTMLGreetingUser(greetings, mobileGreeting, activUser){
   greetings.innerHTML = /*html*/`
-  <p class="font-text-47">${timeOfDay}</p>
-  `;
+      <p class="font-text-47">${timeOfDay},</p>
+      <p class="username-64">${users[activUser].name}</p>
+    `;
+  return mobileGreeting.innerHTML = /*html*/`
+    <p class="font-text-36">${timeOfDay},</p>
+    <p class="username">${users[activUser].name}</p>
+  `; 
 }
