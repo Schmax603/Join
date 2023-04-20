6384e2b05b7b0b51ba53d8a913239d8e475dbe41
Zwogli <Mathiaskohler@web.de>
2023-04-20 07:52:22 +0200

    fix php redirect

--- send_mail.php
+++ send_mail.php
@@ -3,7 +3,7 @@
 ########### CONFIG ###############
 
 $recipient = $_REQUEST["requesterEmail"]; #fill with user email 'requesterEmail'
-$redirect = 'login/forgotPasswordSucces.html'; #loading file after email send
+$redirect = 'forgotPasswordSucces/forgotPasswordSucces.html'; #loading file after email send
 
 ########### CONFIG END ###########
 
