607f45be851fc55d37a1e94cab89a4d118f7589a
Zwogli <Mathiaskohler@web.de>
2023-04-22 07:31:46 +0200

    fix greetings

--- summary/summary.js
+++ summary/summary.js
@@ -6,14 +6,14 @@ let timeOfDay;
 function renderTimeOfDay(){
   let date = new Date;
   let hours = date.getHours();
-  if(hours > 7 && hours < 13){
+  if(hours >= 7 && hours <= 13){
     timeOfDay = 'Good morning';
-  }else if(hours > 13 && hours < 18){
+  }else if(hours >= 13 && hours <= 18){
     timeOfDay = 'Good afternoon';
-  }else if(hours > 18 && hours < 22){
+  }else if(hours >= 18 && hours <= 22){
     timeOfDay = 'Good evening';
   }
-  else if(hours > 22 && hours < 24 || hours > 0 && hours < 7){
+  else if(hours >= 22 && hours <= 24 || hours >= 0 && hours <= 7){
     timeOfDay = 'Good night';
   }
 }
