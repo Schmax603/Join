1838610f19dbcd20295bf62f29844913cd958535
Schmax603 <stark.m.2003@gmail.com>
2023-04-24 14:50:23 +0200

    update

--- addtask/addtask.js
+++ addtask/addtask.js
@@ -1,44 +1,56 @@
-console.log("dadw")
-
 let buttons;
 
-function whichBnt(buttonNum, buttons) {
-
-
+function whichBnt(buttonNum) {
     if (buttonNum === 0) {
-        buttons = document.querySelectorAll('.addtask-prio-bnt-urgent');
-        console.log("urgent", buttonNum)
+        buttonNum = document.querySelectorAll('.addtask-prio-bnt-urgent');
+        buttonNum.id = "0"
     } 
     if (buttonNum === 1) {
-        buttons = document.querySelectorAll('.addtask-prio-bnt-medium');
-        console.log("medium", buttonNum)
+        buttonNum = document.querySelectorAll('.addtask-prio-bnt-medium');
+        buttonNum.id = "1"
     } 
     if (buttonNum === 2) {
-        buttons = document.querySelectorAll('.addtask-prio-bnt-urgent');
-        console.log("low", buttonNum)
+        buttonNum = document.querySelectorAll('.addtask-prio-bnt-low');
+        buttonNum.id = "2"
     } 
+    purgeActive(buttonNum)
 }
+
+function purgeActive(buttonNum) {
+    if (buttonNum.id === "0") {
+        document.getElementById('addtask-prio-bnt-medium').classList.remove('active');
+        document.getElementById('addtask-prio-bnt-low').classList.remove('active');
+    }
+    if (buttonNum.id === "1") {
+        document.getElementById('addtask-prio-bnt-low').classList.remove('active');
+        document.getElementById('addtask-prio-bnt-urgent').classList.remove('active');
+    }
+    if (buttonNum.id === "2") {
+        document.getElementById('addtask-prio-bnt-medium').classList.remove('active');
+        document.getElementById('addtask-prio-bnt-urgent').classList.remove('active');
+    }
+    isActive(buttonNum)
+}
+
 
 function isActive(buttonNum) {
-    buttons.forEach(button => {
-        if (button.id === `button-${buttonNum}`) {
-
-            console.log(button.id)
-            button.classList.toggle('active');
-            console.log("add", buttonNum)
-
-        } else {
-
-            console.log(button.id)
-            button.classList.remove('active');
-            console.log("remove", buttonNum)
-
-        }
-    });
+    buttonNum[0].classList.toggle('active');
 }
 
+function hover(params) {
+    
+}
+
+
+
 function toggleActive() {
-    document.getElementById("collapsible").classList.toggle("active")
+
+    for (let i = 0; i < 2; i++) {
+
+        document.getElementById("collapsible`${i}`").classList.toggle("active")
+        document.getElementById("collapsible`${i}`").classList.toggle("active")
+
+    }
 }
 
 function dropdownValueCheck() {
@@ -50,17 +62,4 @@ function dropdownValueCheck() {
         return dropid.length
     }
 
-}
-
-function loadCategory() {
-    const coll = document.getElementById("apicategory");
-    const divs = coll.getElementsByTagName("div");
-
-
-}
-
-function newCategory() {
-    const coll = document.getElementById("apicategory");
-    const divs = coll.getElementsByTagName("div");
-    const currentItem = document.getElementById("currentItem")
 }
